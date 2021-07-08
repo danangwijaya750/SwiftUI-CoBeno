f3454aa56adcf2bdbe9fb826c94a62e4ebd9eacc
Danang Wijaya <danangwijaya750@gmail.com>
2021-07-08 12:30:56 +0700

    Initial Commit

---
+++ Co-Beno/Co_BenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Co_BenoApp.swift
+//  Co-Beno
+//
+//  Created by Danang Wijaya on 08/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Co_BenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
