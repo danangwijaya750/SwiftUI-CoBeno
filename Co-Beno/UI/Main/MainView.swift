efc8ae1980611d857104df8c27bab5af3d337b46
Danang Wijaya <danangwijaya750@gmail.com>
2021-07-09 21:20:11 +0700

    finish get data from BE

--- Co-Beno/UI/Main/MainView.swift
+++ Co-Beno/UI/Main/MainView.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import SwiftUI
-
+import ToastUI
 struct MainView: View {
     @ObservedObject var viewModel = MainViewModel()
     @State var province = ""
@@ -20,15 +20,46 @@ struct MainView: View {
                             viewModel.getAvailableBeds(province: q)
                         }
                     }
-                    List(viewModel.availableBeds){ item in
-                        VStack{
-                            Text(item.name).font(.headline)
+                        List(viewModel.availableBeds){ item in
+                            RowItem(data: item)
                         }
-                    }
                 }
-                ProgressView().isHidden(viewModel.onLoading)
-                }.navigationTitle("Covid-19 Bed Info")
+                ProgressView().isHidden(!viewModel.onLoading)
             }
+            .navigationTitle("Covid-19 Bed Info")
+            }
+    }
+}
+
+struct RowItem: View {
+    let data: BedInfo
+    var body: some View{
+        VStack(alignment:.leading,spacing: 5.0){
+            HStack(spacing:10){
+                Image(systemName: "stethoscope")
+                Text(data.name).font(.headline)
+            }
+            HStack(spacing:10){
+                Image(systemName: "location.fill")
+                Text(data.address).font(.body)
+            }
+            HStack(spacing:10){
+                Image(systemName: "bed.double.fill")
+                Text("Tersedia : \(data.available_bed)").foregroundColor((data.available_bed > 0) ? .green : .red)
+            }
+            HStack(spacing:10){
+                Image(systemName: "square.stack.3d.down.right.fill")
+                Text("Antrian : \(data.bed_queue)").foregroundColor((data.bed_queue > 0) ? .green : .red)
+            }
+            HStack(spacing:10){
+                Image(systemName: "phone.fill")
+                if(data.hotline.count>0){
+                    Text(data.hotline)
+                }else{
+                    Text("Not Available")
+                }
+            }
+        }
     }
 }
 
--- Co-Beno/UI/Main/MainViewModel.swift
+++ Co-Beno/UI/Main/MainViewModel.swift
@@ -10,7 +10,8 @@ import Alamofire
 
 class MainViewModel: ObservableObject {
     @Published var availableBeds = [BedInfo]()
-    @Published var onLoading = true
+    @Published var onLoading = false
+    @Published var isError = ""
     func getAvailableBeds(province: String){
         self.onLoading = true
         let url = "https://ina-covid-bed.vercel.app/api/bed?prov=\(province.lowercased())"
@@ -18,13 +19,17 @@ class MainViewModel: ObservableObject {
             .responseJSON{res in
                 if let json = res.value{
                     print(json)
-                    if let data = json as? Response{
-                        if(data.status == 200){
-                            print("here")
-                            self.onLoading=false
-                            data.data.forEach{item in
-                                self.availableBeds.append(item)
+                    if let data = json as? Dictionary<String, AnyObject?>{
+                        if let responseCode = data["status"] as? Int, let items = data["data"] as? Array<Dictionary<String,AnyObject?>>{
+                            if responseCode==200{
+                                self.isError=""
+                                self.onLoading=false
+                                items.forEach{item in
+                                    self.availableBeds
+                                        .append(BedInfo(name: (item["name"] as? String)!, address: (item["address"] as? String)!, available_bed: (item["available_bed"] as? Int)!, bed_queue: (item["bed_queue"] as? Int)!, hotline: (item["hotline"] as? String)!, bed_detail_link: (item["bed_detail_link"] as? String)!, hospital_code: (item["hospital_code"] as? String)!, updated_at_minutes: (item["updated_at_minutes"] as? Int)!, lat: (item["lat"] as? String)!, lon: (item["lon"] as? String)!))
+                                }
                             }
+                            
                         }
                     }
                 }
